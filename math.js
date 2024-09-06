496104c815298a125ed45ab422a49f84b69d7e89
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-07 01:59:11 +0600

    All Files Done and Completed

---
+++ math.js
@@ -0,0 +1,26 @@
+// const Math = {
+//     min: function min(num1, num2) {
+//         return num2;
+//     }
+// }
+
+// console.log(Math.min);
+
+const min = Math.min(45, 21, 65, 69, 88, -92, 55, 23, 85);
+console.log(min);
+const max = Math.max(45, 21, 65, 69, 88, -92, 55, 23, 85);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(5 - 10));
+console.log(Math.abs(10 - 5));
+console.log(Math.round(6.69));
+console.log(Math.round(6.49));
+console.log(Math.floor(6.69));
+console.log(Math.ceil(2.005));
+console.log(Math.ceil(2.999));
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
