bdc976b057e65c484d8b6be815db2437e489ba73
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-16 23:26:03 +0600

    Tasks JS File Updated

--- tasks.js
+++ tasks.js
@@ -5,7 +5,6 @@
 
 
 
-
 // Task - 02
 
 
@@ -21,47 +20,3 @@
 
 
 // Task - 05
-
-
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-
-
-function main() {
-    const n = parseInt(readLine().trim(), 10);
-
-    // code here
-    // Check if the number is negative
-    if (n < 0) {
-        console.log("Number is negative");
-    } else {
-        // Check if the number is even or odd
-        if (n % 2 === 0) {
-            console.log("Even");
-        } else {
-            console.log("Odd");
-        }
-    }
-}
-
-console.log(main(4));
